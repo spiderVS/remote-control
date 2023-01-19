412abbdf6c74f8a00fd28fe869441c450b38daf8
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2023-01-19 21:15:51 +0300

    feat: move project to typescript

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from "./src/http_server/index";
 import { mouse } from "@nut-tree/nut-js";
+import { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({port: 8080});
+
+
