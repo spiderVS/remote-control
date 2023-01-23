92f7d8670b855cb3d7be6cf5fd42562e6a47bed8
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2023-01-23 18:22:25 +0300

    feat: add correct end websocket work

--- src/websocket-server/index.ts
+++ src/websocket-server/index.ts
@@ -1,17 +1,26 @@
 import { createWebSocketStream, WebSocketServer } from "ws";
 import { runCommand } from "../cmd/commands";
+import { httpServer } from '../http_server/index';
 
 export const startWsServer = ( port: number ) => {
+  let isSIGINT = false;
+
   const wsServer = new WebSocketServer({ port: port });
 
   wsServer.on('listening', () => {
     console.log(`\nWebSocket Server: Listening on ws://localhost:${port}\n`);
   });
 
+  wsServer.on('close', () => {
+    console.log('WebSocketServer has been closed successfully.\n');
+    httpServer.close();
+  });
+
   wsServer.on('connection', (ws) => {
-    console.log(`WebSocket Server: Client connected`);
 
     const duplex = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false });
+    console.log(`WebSocket Server: Client connected\n`);
+    duplex.write('Connected');
 
     duplex.on('readable', async () => {
       let data;
@@ -21,14 +30,13 @@ export const startWsServer = ( port: number ) => {
         if (result) {
           if (result.startsWith('prnt_scrn')) {
             console.log('WebSocket Server -> prnt_scrn <base64 string (png buf)>');
-          } else {
+          } else if (result.startsWith('mouse_position')) {
             console.log('WebSocket Server -> %s', result);
           }
           duplex.write(result);
 
         } else {
           duplex.write(data);
-          console.log('WebSocket Server -> %s', data);
         }
       }
     });
@@ -37,15 +45,17 @@ export const startWsServer = ( port: number ) => {
       duplex.end();
     });
 
-
-    ws.on('close', () => {
-      ws.close();
+    ws.on('close', (code, reason) => {
+      console.log('WebSocket has been closed sucessfully.');
+      if (isSIGINT) {
+        wsServer.close();
+      }
     });
 
-    ws.send('Connected');
+    process.on('SIGINT', () => {
+      ws.close();
+      isSIGINT = true;
+    });
   });
 
-  wsServer.on('close', () => {
-    console.log('The connection has been closed successfully.');
-  });
 };
